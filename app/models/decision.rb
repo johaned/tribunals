70ff530273a223b45f110561a7c222d1e4177858
James Darling <james@abscond.org>
2013-02-21 22:25:46 +0000

    Decisions will now (slightly crudely for now) convert it's .doc to html on creation

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -2,4 +2,13 @@ class Decision < ActiveRecord::Base
   attr_accessible :doc_file, :html, :pdf_file, :promulgated_on
   mount_uploader :doc_file, DocFileUploader
   mount_uploader :pdf_file, PdfFileUploader
+
+  before_create :process_doc
+
+  def process_doc
+    tmp_html_dir = File.join(Rails.root, 'tmp')
+    `/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to html --outdir #{tmp_html_dir} #{self.doc_file.file.path}`
+    html_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '.html'))
+    self.html = File.read(html_file)
+  end
 end
