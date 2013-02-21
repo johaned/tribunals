d89d242caf80adc3d16e08ceace4f2089afc1879
James Darling <james@abscond.org>
2013-02-21 14:49:03 +0000

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tribunals::Application.config.secret_token = 'f59441fe388777872f02713b37ceb85dbbb3c2574c62ce17c8d3b6341653f895ba8a14e5cff4c39be0f4bbe4e15fba32701e88aa36607f4134445ed19fb7fdf3'
