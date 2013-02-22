e5bf7565eee9baad4f94247c1a466213fe0a3c10
James Darling <james@abscond.org>
2013-02-22 11:29:23 +0000

    very basic admin for uploading decisions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Tribunals::Application.routes.draw do
+  namespace :admin do
+    resources :decisions
+  end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
