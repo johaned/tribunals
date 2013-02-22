fb2424723b4b9183901bbf96e642af0d046d2ebe
James Darling <james@abscond.org>
2013-02-22 09:46:46 +0000

    convert .doc to pdf file on decisions

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -7,8 +7,12 @@ class Decision < ActiveRecord::Base
 
   def process_doc
     tmp_html_dir = File.join(Rails.root, 'tmp')
-    `/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to html --outdir #{tmp_html_dir} #{self.doc_file.file.path}`
+    [:pdf, :html].each do |type|
+      `/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to #{type} --outdir #{tmp_html_dir} #{self.doc_file.file.path}`
+    end
     html_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '.html'))
+    pdf_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '.pdf'))
     self.html = File.read(html_file)
+    self.pdf_file = File.open(pdf_file)
   end
 end
