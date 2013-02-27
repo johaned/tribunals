b556c87a2b1784350b8f750f598a7fe78bd26d81
James Darling <james@abscond.org>
2013-02-27 18:34:04 +0000

    added specs for html_body and raw text extraction. Includes a fix (found by tests!) for html_body.

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -3,24 +3,24 @@ class Decision < ActiveRecord::Base
   mount_uploader :doc_file, DocFileUploader
   mount_uploader :pdf_file, PdfFileUploader
 
-  validates_presence_of :doc_file
-
   before_validation :process_doc, :on => :create
 
   def process_doc
-    tmp_html_dir = File.join(Rails.root, 'tmp')
-    [:pdf, :html].each do |type|
-      `/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to #{type} --outdir #{tmp_html_dir} #{self.doc_file.file.path}`
+    if doc_file.present?
+      tmp_html_dir = File.join(Rails.root, 'tmp')
+      [:pdf, :html].each do |type|
+        `/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to #{type} --outdir #{tmp_html_dir} #{self.doc_file.file.path}`
+      end
+      html_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '')+'.html')
+      pdf_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '')+'.pdf')
+      self.html = File.read(html_file)
+      self.pdf_file = File.open(pdf_file)
+      self.set_text_from_html
     end
-    html_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '')+'.html')
-    pdf_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '')+'.pdf')
-    self.html = File.read(html_file)
-    self.pdf_file = File.open(pdf_file)
-    self.set_text_from_html
   end
 
   def html_body
-    Nokogiri::HTML(self.html).css('body *').to_html
+    Nokogiri::HTML(self.html).css('body').children.to_html
   end
 
   def set_text_from_html
