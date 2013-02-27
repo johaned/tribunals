71d75f4fbed157dad46fec28538ba7e39b8d0ff4
James Darling <james@abscond.org>
2013-02-27 13:00:57 +0000

    Extract raw text from decisions

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -16,9 +16,14 @@ class Decision < ActiveRecord::Base
     pdf_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '')+'.pdf')
     self.html = File.read(html_file)
     self.pdf_file = File.open(pdf_file)
+    self.set_text_from_html
   end
 
   def html_body
     Nokogiri::HTML(self.html).at_css('body').to_html
+  end
+
+  def set_text_from_html
+    self.text = Nokogiri::HTML(self.html).at_css('body').text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130221154317) do
+ActiveRecord::Schema.define(:version => 20130227124449) do
 
   create_table "decisions", :force => true do |t|
     t.string   "doc_file"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20130221154317) do
     t.string   "pdf_file"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+    t.text     "text"
   end
 
 end
