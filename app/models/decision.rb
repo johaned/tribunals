78c3db675579861237cc707cef97331c654f8e3f
James Darling <james@abscond.org>
2013-02-27 11:24:50 +0000

    Carrierwave doesn't work on before_create, so moved decision processing to after_validation on create.

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -5,15 +5,15 @@ class Decision < ActiveRecord::Base
 
   validates_presence_of :doc_file
 
-  before_create :process_doc
+  before_validation :process_doc, :on => :create
 
   def process_doc
     tmp_html_dir = File.join(Rails.root, 'tmp')
     [:pdf, :html].each do |type|
       `/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to #{type} --outdir #{tmp_html_dir} #{self.doc_file.file.path}`
     end
-    html_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '.html'))
-    pdf_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '.pdf'))
+    html_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '')+'.html')
+    pdf_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '')+'.pdf')
     self.html = File.read(html_file)
     self.pdf_file = File.open(pdf_file)
   end
