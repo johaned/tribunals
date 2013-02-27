9cc09c64b97f17f5adccf7208057cff3d87d187e
James Darling <james@abscond.org>
2013-02-27 18:33:15 +0000

    remove unused specs

--- spec/controllers/decisions_controller_spec.rb
+++ spec/controllers/decisions_controller_spec.rb
@@ -3,17 +3,9 @@ require 'spec_helper'
 describe DecisionsController do
 
   describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
   end
 
   describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
-    end
   end
 
 end
