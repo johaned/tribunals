3afb1a9aff5a90161c936436aab7e52330fc5fd6
James Darling <james@abscond.org>
2013-02-28 17:04:17 +0000

    Give up on slimmer for the time being and just copy paste the layout directly.

--- config/application.rb
+++ config/application.rb
@@ -58,8 +58,5 @@ module Tribunals
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
-
-    config.slimmer.asset_host = "https://static.production.alphagov.co.uk/"
-
   end
 end
