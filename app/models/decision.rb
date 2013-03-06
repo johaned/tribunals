72a82a1bc0aaef8f9b3351d5fdeb1892b978fee5
James Darling <james@abscond.org>
2013-03-06 13:09:37 +0000

    use suffice from path, and print response to console

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -9,7 +9,7 @@ class Decision < ActiveRecord::Base
     if doc_file.present?
       tmp_html_dir = File.join(Rails.root, 'tmp')
       [:pdf, :html].each do |type|
-        `/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to #{type} --outdir #{tmp_html_dir} #{self.doc_file.file.path}`
+        p `soffice --headless --convert-to #{type} --outdir #{tmp_html_dir} #{self.doc_file.file.path}`
       end
       html_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '')+'.html')
       pdf_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '')+'.pdf')
