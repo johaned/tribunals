8f063a43ea9fa78505940e380ab12589402811a8
James Darling <james@abscond.org>
2013-04-15 22:30:48 +0100

    * Switch to bootstrap for styling * use radio buttons instead of checkbox for reported

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -1,7 +1,7 @@
 class DecisionsController < ApplicationController
   def index
     params[:search] ||= {}
-    params[:search][:reported] = nil if params[:search][:reported] == '0'
+    params[:search][:reported]
     params[:search][:country_guideline] = nil if params[:search][:country_guideline] == '0'
     @decisions = Decision.paginate(:page => params[:page], :per_page => 30).ordered
     if params[:search].present?
