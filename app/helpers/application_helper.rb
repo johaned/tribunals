c529073537757408ee122b6cbabaac5fdc27d963
James Darling <james@abscond.org>
2013-04-23 15:13:32 +0100

    Hilight search results

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def hilighted_search_result(search_term, text)
+		excerpt = excerpt(text, search_term, :radius => 50)
+		highlight(excerpt, search_term, :highlighter => '<span class=\'result\'>\1</span>')
+	end
 end
