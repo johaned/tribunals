d297a1ee943b118e169ae90696195086d95588ed
Jan Szumiec <jan.szumiec@gmail.com>
2013-06-03 17:08:39 +0100

    Run document conversion in parallel.

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -67,11 +67,15 @@ class Decision < ActiveRecord::Base
   end
 
   def process_doc
+    require 'thwait'
     if doc_file.present?
       tmp_html_dir = File.join(Rails.root, 'tmp')
-      [:pdf, :html].each do |type|
-        `soffice --headless --convert-to #{type} --outdir #{tmp_html_dir} #{self.doc_file.file.path}`
+      threads = [:pdf, :html].map do |type|
+        Thread.new do
+          `soffice --headless --convert-to #{type} --outdir #{tmp_html_dir} #{self.doc_file.file.path}`
+        end
       end
+      ThreadsWait.all_waits(*threads)
       html_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '')+'.html')
       pdf_file = File.join(tmp_html_dir, doc_file.file.filename.gsub('.doc', '')+'.pdf')
       self.html = File.read(html_file)
