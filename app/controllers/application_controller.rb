887ac7afdbaf8c75f2b300599df8590dc5e3d6d6
Jan Szumiec <jan.szumiec@gmail.com>
2013-06-10 11:29:54 +0100

    Basic auth is no longer needed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  before_filter :authenticate unless Rails.env.test?
-
-  protected
-
-  def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == "prototype" && password == "will_be_buggy"
-    end
-  end
 end
