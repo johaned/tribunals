8360c4ccde0b5db59d9fede184561fbb84b639ab
Jan Szumiec <jan.szumiec@gmail.com>
2013-07-05 17:48:08 +0100

    Bucket name is configurable via an environment variable.

--- spec/support/fog.rb
+++ spec/support/fog.rb
@@ -1,4 +1,7 @@
 Fog.mock!
+CarrierWave.configure do |config|
+  config.fog_directory = 'just-testing'
+end
 connection = Fog::Storage.new(:provider => 'AWS')
-connection.directories.create(:key => 'tribunals.cjs.gov.uk')
+connection.directories.create(:key => 'just-testing')
 
