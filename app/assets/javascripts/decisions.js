935e415aa2b828d2ef6a3b3ce40e2c667e0ffbea
Jan Szumiec <jan.szumiec@gmail.com>
2013-07-11 17:20:09 +0100

    The button for clearing the form works.

--- app/assets/javascripts/decisions.js
+++ app/assets/javascripts/decisions.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function() {
+  $('button[type=reset]').click(function(evt) {
+    evt.preventDefault();
+    $(this).closest('form').find('input[type=text]').val('');
+  });
+});
