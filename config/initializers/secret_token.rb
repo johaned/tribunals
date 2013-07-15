601d307507bd42ddc09f3a6916a2925f7c2fe815
Jan Szumiec <jan.szumiec@gmail.com>
2013-07-15 15:37:08 +0100

    Session token passthrough.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Tribunals::Application.config.secret_token = 'f59441fe388777872f02713b37ceb85dbbb3c2574c62ce17c8d3b6341653f895ba8a14e5cff4c39be0f4bbe4e15fba32701e88aa36607f4134445ed19fb7fdf3'
+Tribunals::Application.config.secret_token = Rails.env.production? ? ENV['SESSION_SECRET_KEY'] : "d42be296c9ecd4e4ca9b3162303ba8d9"
+
+
