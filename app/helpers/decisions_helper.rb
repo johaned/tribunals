0312aabb28a3c73696f0ccf3f741f712f2df93af
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2013-07-16 18:16:06 +0100

    markup judge names as authors

--- app/helpers/decisions_helper.rb
+++ app/helpers/decisions_helper.rb
@@ -15,4 +15,8 @@ module DecisionsHelper
     ("<time property='datePublished' timedate='#{date.to_formatted_s(:rfc3339)}'>#{date.to_formatted_s(:rfc822)}</time>").html_safe
   end
 
+  def schema_author_span(author)
+    ("<span property='author'>#{author}</span>").html_safe
+  end
+
 end
