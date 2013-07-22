7136eba6c763b081a1cdd431fdf7d74377d6f874
Jan Szumiec <jan.szumiec@gmail.com>
2013-07-22 12:04:04 +0100

    TRIBS-26 When a search term doesn't appear in the decision, the app doesn't blow up.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,10 +2,17 @@ require "spec_helper"
 
 describe ApplicationHelper do
   describe "#hilighted_search_result" do
+    let(:text) do
+      "This the the article that contains the search term This the the article that contains the search term This the the article that contains the search term This the the article that contains the search term This the the article that contains the search term This the the article that contains the search term This the the article that contains the search term"
+    end
+
     it "excerpts and hilights the result from the text" do
-      text = "This the the article that contains the search term This the the article that contains the search term This the the article that contains the search term This the the article that contains the search term This the the article that contains the search term This the the article that contains the search term This the the article that contains the search term"
       helper.hilighted_search_result("contains", text).should ==
       	"This the the article that <span class='result'>contains</span> the search term This the the article that contain..."
+    end
+
+    it "doesn't blow up when the text to be highlighted doesn't appear in the decision" do
+      helper.hilighted_search_result("blah blah blah", text).should be_nil
     end
   end
 end
