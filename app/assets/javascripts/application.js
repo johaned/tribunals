60bbd312b410de08d26fb44171a25dff81014db9
Clive Murray <clive.murray@digital.justice.gov.uk>
2013-07-23 14:54:16 +0100

    Add/remove judges, keywords, categories in admin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,4 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require_tree .
+//= require_tree ../vendor
+//= require moj
+//= require decisions
+//= require admin/decisions
+//= require main
