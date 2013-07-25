4a25f403171cdcddbcb1f59dd663d405c2fa3ec7
Jan Szumiec <jan.szumiec@gmail.com>
2013-07-25 11:53:34 +0100

    Create indices concurrently.

--- db/migrate/20130724093005_even_better_fts_indices.rb
+++ db/migrate/20130724093005_even_better_fts_indices.rb
@@ -3,7 +3,7 @@ class EvenBetterFtsIndices < ActiveRecord::Migration
     execute "DROP INDEX decisions_fts_idx_before_jun1"
     execute "DROP INDEX decisions_fts_idx_after_jun1"
 
-    execute "CREATE INDEX decisions_fts_idx_after_jun1 ON decisions USING gin(to_tsvector('english', text)) WHERE promulgated_on >= '2013-06-01' OR reported = 't'"
-    execute "CREATE INDEX decisions_fts_idx ON decisions USING gin(to_tsvector('english', text))"
+    execute "CREATE INDEX CONCURRENTLY decisions_fts_idx_after_jun1 ON decisions USING gin(to_tsvector('english', text)) WHERE promulgated_on >= '2013-06-01' OR reported = 't'"
+    execute "CREATE INDEX CONCURRENTLY decisions_fts_idx ON decisions USING gin(to_tsvector('english', text))"
   end
 end
