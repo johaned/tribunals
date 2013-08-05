c027f7fe49629e8669610607ff072b2d9d4b3b3b
Clive Murray <clive.murray@digital.justice.gov.uk>
2013-08-05 13:13:04 +0100

    TRIBS-49 Fix case title so that the other fields don't shift up on show

--- app/helpers/decisions_helper.rb
+++ app/helpers/decisions_helper.rb
@@ -23,4 +23,8 @@ module DecisionsHelper
     ("<span property='author'>#{author}</span>").html_safe
   end
 
+  def case_title(decision)
+    decision.case_name || decision.claimant + ' (' + decision.keywords.join(', ').html_safe + ')'
+  end
+
 end
