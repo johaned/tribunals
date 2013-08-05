e29641de745ad01f056a27980d7d549e46da0fdf
Jan Szumiec <jan.szumiec@gmail.com>
2013-08-05 14:26:12 +0100

    TRIBS-58, TRIBS-61 Search term highlighting, search result ordering.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def hilighted_search_result(search_term, text)
-    if excerpt = excerpt(text, search_term, :radius => 50)
+    if excerpt = excerpt(text, search_term, radius: 5, separator: ' ')
       highlight(excerpt, search_term, :highlighter => '<span class=\'result\'>\1</span>')
     end
   end
@@ -10,18 +10,6 @@ module ApplicationHelper
       [@page_title, 'Upper Tribunal (Immigration and Asylum Chamber) Decision Database'].join(' | ')
     else
       'Upper Tribunal (Immigration and Asylum Chamber) Decision Database'
-    end
-  end
-
-  # Fast, but inexact reimplementation of excerpt() from Rails.
-  def excerpt(text, search_term, options={})
-    return if search_term.blank? || text.blank?
-    radius = options.fetch(:radius, 10)
-
-    if index = text.index(search_term)
-      lower_bound = [index - radius, 0].max
-      upper_bound = index + search_term.length + radius
-      [lower_bound == 0 ? '' : '...', text[lower_bound..upper_bound], upper_bound == text.length ? '' : '...'].join
     end
   end
 end
