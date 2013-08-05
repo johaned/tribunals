1f4e668d0c2638c9b83262185c662155cd22a065
Jan Szumiec <jan.szumiec@gmail.com>
2013-08-05 14:26:12 +0100

    TRIBS-58, TRIBS-61 Search term highlighting, search result ordering.

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -3,9 +3,11 @@ class DecisionsController < ApplicationController
     params[:search] ||= {}
     params[:search][:reported]
     params[:search][:country_guideline] = nil if params[:search][:country_guideline] == '0'
-    @decisions = self.class.scope.paginate(:page => params[:page], :per_page => 30).ordered
+    @decisions = self.class.scope.paginate(:page => params[:page], :per_page => 30)
     if params[:search].present?
       @decisions = @decisions.filtered(params[:search])
+    else
+      @decisions = @decisions.ordered
     end
   end
 
