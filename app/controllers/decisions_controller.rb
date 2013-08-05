f94ddef5516cd70167ddd56d4103c110a01346da
Clive Murray <clive.murray@digital.justice.gov.uk>
2013-08-05 13:13:04 +0100

    TRIBS-49 Fix case title so that the other fields don't shift up on show

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -11,7 +11,7 @@ class DecisionsController < ApplicationController
 
   def show
     @decision = self.class.scope.find(params[:id])
-    @page_title = @decision.label
+    @page_title = @decision.case_name || @decision.claimant + ' (' + @decision.keywords.join(', ').html_safe + ')'
   end
 
   def self.scope
