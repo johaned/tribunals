35d1a9e2a6b7aa6d4efa06eee6c5f7556bc9f3c0
Jan Szumiec <jan.szumiec@gmail.com>
2013-08-07 13:50:52 +0100

    TRIBS-80 Only whole word matches are highlighted.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
   def hilighted_search_result(search_term, text)
-    if excerpt = excerpt(text, search_term, radius: 5, separator: ' ')
-      highlight(excerpt, search_term, :highlighter => '<span class=\'result\'>\1</span>')
+    search_regexp = /\b#{Regexp.escape(search_term)}\b/i
+    if excerpt = excerpt(text, search_regexp, radius: 5, separator: ' ')
+      highlight(excerpt, search_regexp, :highlighter => '<span class=\'result\'>\1</span>')
     end
   end
 
@@ -11,5 +12,51 @@ module ApplicationHelper
     else
       'Upper Tribunal (Immigration and Asylum Chamber) Decision Database'
     end
+  end
+
+  # Backports from bleeding edge rails.
+  def excerpt(text, phrase, options = {})
+    return unless text && phrase
+
+    separator = options.fetch(:separator, "")
+    if Regexp === phrase
+      regex = phrase
+    else
+      phrase    = Regexp.escape(phrase)
+      regex     = /#{phrase}/i
+    end
+
+    return unless matches = text.match(regex)
+    phrase = matches[0]
+
+    unless separator.empty?
+      text.split(separator).each do |value|
+        if value.match(regex)
+          regex = phrase = value
+          break
+        end
+      end
+    end
+
+    first_part, second_part = text.split(regex, 2)
+
+    prefix, first_part   = cut_excerpt_part(:first, first_part, separator, options)
+    postfix, second_part = cut_excerpt_part(:second, second_part, separator, options)
+
+    prefix + (first_part + separator + phrase + separator + second_part).strip + postfix
+  end
+
+  def highlight(text, phrases, options = {})
+    text = sanitize(text) if options.fetch(:sanitize, true)
+
+    if text.blank? || phrases.blank?
+      text
+    else
+      highlighter = options.fetch(:highlighter, '<mark>\1</mark>')
+      match = Array(phrases).map do |p|
+        Regexp === p ? p.to_s : Regexp.escape(p)
+      end.join('|')
+      text.gsub(re = /(#{match})(?![^<]*?>)/i, highlighter)
+    end.html_safe
   end
 end
