3097c24fd5f74befae2ec0c8def8409c0b8b7a06
Jan Szumiec <jan.szumiec@gmail.com>
2013-08-19 10:31:37 +0100

    Disabled application-level SSL check - not needed anymore.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  force_ssl if: :ssl_configured?
-
-  def ssl_configured?
-    headers['X-Forwarded-Proto'] == 'https'
-  end
 end
