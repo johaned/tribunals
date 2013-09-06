6e6dfe2b87c1165bb4478fb97a332d587e395c58
Hrishi Mittal <hrishimittal@gmail.com>
2013-09-06 17:21:18 +0100

    TRIBS-123 Add feedback form
    Fix feedback form javascript bugs by moving to separate js includes. Add form styling.
    
    Fix whitespace
    
    Remove CSRF tag from layout. Refactor tests.

---
+++ app/mailers/notifications_mailer.rb
@@ -0,0 +1,9 @@
+class NotificationsMailer < ActionMailer::Base
+
+  default to: ENV['FEEDBACK_EMAIL_RECEIVER'], from: "no-reply@tribunalsdecisions.service.gov.uk"
+
+  def new_message(message)
+    @message = message
+    mail(:subject => "New feedback for Tribunals")
+  end
+end
