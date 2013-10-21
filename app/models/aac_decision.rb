dce359a056821817d7545fe0974cc186b66e9c13
Hrishi Mittal <hrishimittal@gmail.com>
2013-10-21 17:20:51 +0100

    Add AAC decisions document files

--- app/models/aac_decision.rb
+++ app/models/aac_decision.rb
@@ -4,4 +4,7 @@ class AacDecision < ActiveRecord::Base
   has_many :aac_judgements
   has_many :judges, through: :aac_judgements
   has_many :aac_import_errors
+
+  mount_uploader :doc_file, DocFileUploader
+  mount_uploader :pdf_file, PdfFileUploader
 end
