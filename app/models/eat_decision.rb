5ea86a8e0c3f8b48804841f7b2e37ed4d53a6260
Aleksandar Simic <alex.simic@digital.justice.gov.uk>
2013-11-01 15:37:20 +0000

    EAT doc fetching & processing

--- app/models/eat_decision.rb
+++ app/models/eat_decision.rb
@@ -1,8 +1,24 @@
+require 'doc_processor'
+
 class EatDecision < ActiveRecord::Base
   has_many :eat_category_decisions
   has_many :eat_subcategories, through: :eat_category_decisions
 
+  mount_uploader :doc_file, DocFileUploader
+  mount_uploader :doc_file, PdfFileUploader
+
   def self.ordered
     order("hearing_date DESC")
   end
+
+  def add_doc
+    if doc = Dir.glob(File.join("#{Rails.root}/data/eat/downloads/#{id}", "*.doc")).first
+      DocProcessor.add_doc_file(self, File.open(doc))
+    end
+  end
+
+  def process_doc
+    DocProcessor.process_doc_file(self, doc_file) if doc_file.present?
+  end
+
 end
